6202e4ed0ca16c146011e55594b43a61ed938d8d
HornetPlay <KasperP233@gmail.com>
2019-04-08 20:05:35 +0300

    modal window in catalog

--- script/script.js
+++ script/script.js
@@ -3,7 +3,9 @@
   	 	interval:3500,
   	 	height: 640
   	 });
-       $('.sidenav').sidenav();
+    $('.sidenav').sidenav();
+    $('.modal').modal();
+    $('input#input_text, textarea#textarea2').characterCounter();
 
   });
 
